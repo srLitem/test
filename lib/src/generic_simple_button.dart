aebc4393ee75c777d4cfa5bdc7bfd92303511900
Santiago RR <santiago.rhenalsr@pragma.com.co>
2024-05-10 08:47:41 -0500

    Finishing PoC

---
+++ lib/src/generic_simple_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class GenericSimpleButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final VoidCallback onPressed;
+
+  const GenericSimpleButton({super.key, 
+    required this.text,
+    required this.color,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
